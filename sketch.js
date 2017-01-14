d6a1fb2ea821d7496019f057b03f92e3b933e5d0
Jakub Pustelnik <jhirtiganto@gmail.com>
2017-01-14 15:17:22 +0100

    Added redrawBackground()

--- sketch.js
+++ sketch.js
@@ -3,5 +3,13 @@ function setup() {
 }
 
 function draw() {
+  redrawBackground()
+}
+
+function redrawBackground() {
   background(66, 244, 209)
+
+  noStroke()
+  fill(135, 93, 119)
+  rect(0, height - 60, width, 60)
 }
