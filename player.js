3e1d70273d07a9972811447d4b29bdec9b15881a
Jakub Pustelnik <jhirtiganto@gmail.com>
2017-01-14 17:22:08 +0100

    Added jumping and increased readability of the code

--- player.js
+++ player.js
@@ -4,6 +4,10 @@ function Player() {
   this.velocity = createVector(0, 0)
   this.speedScale = 10
 
+  this.jumped = false
+  this.jumpHeight = createVector(0, -200)
+  this.gravity = (0, 1)
+
   this.applyForce = function(force) {
     this.acceleration.add(force)
   }
@@ -17,17 +21,35 @@ function Player() {
     } else if (keyIsDown(RIGHT_ARROW)) {
       dir.mult(this.speedScale)
       this.applyForce(dir);
+    } else if (keyIsDown(32) && !this.jumped) {
+      console.log("jumped")
+      this.jumped = true
     }
   }
 
   this.update = function() {
     this.move()
 
+    if (this.jumped) {
+      this.jumpHeight.mult(0.9)
+      this.applyForce(this.jumpHeight)
+    }
+
     this.velocity.add(this.acceleration)
     this.location.add(this.velocity)
 
     this.acceleration.mult(0)
     this.velocity.mult(0)
+
+    if (this.location.y < height - 180) {
+      this.gravity.mult(1.1)
+      this.applyForce(this.gravity)
+    } else {
+      this.jumpHeight = createVector(0, -45)
+      this.gravity = createVector(0, 1)
+      this.jumped = false
+      this.location.y = height - 180
+    }
   }
 
   this.render = function() {
