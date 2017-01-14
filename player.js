7bf694e12a2fb86101c566f33d99180d2d7499c1
Jakub Pustelnik <jhirtiganto@gmail.com>
2017-01-14 15:50:16 +0100

    Added code that's responsible for moving

--- player.js
+++ player.js
@@ -1,5 +1,34 @@
 function Player() {
   this.location = createVector(width / 2, height - 180)
+  this.acceleration = createVector(0, 0)
+  this.velocity = createVector(0, 0)
+  this.speedScale = 10
+
+  this.applyForce = function(force) {
+    this.acceleration.add(force)
+  }
+
+  this.move = function() {
+    var dir = createVector(1, 0)
+
+    if (keyIsDown(LEFT_ARROW)) {
+      dir.mult(-this.speedScale)
+      this.applyForce(dir);
+    } else if (keyIsDown(RIGHT_ARROW)) {
+      dir.mult(this.speedScale)
+      this.applyForce(dir);
+    }
+  }
+
+  this.update = function() {
+    this.move()
+
+    this.velocity.add(this.acceleration)
+    this.location.add(this.velocity)
+
+    this.acceleration.mult(0)
+    this.velocity.mult(0)
+  }
 
   this.render = function() {
     noStroke()
